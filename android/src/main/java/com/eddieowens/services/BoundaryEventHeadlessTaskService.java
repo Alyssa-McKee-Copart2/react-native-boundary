37e0bcb5472d3b9bb647b42abfc7142c58537da2
Alyssa McKee <alyssa.mckee@copart.com>
2024-04-04 16:52:10 -0500

    added missing imports and color resources

--- android/src/main/java/com/eddieowens/services/BoundaryEventHeadlessTaskService.java
+++ android/src/main/java/com/eddieowens/services/BoundaryEventHeadlessTaskService.java
@@ -5,6 +5,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
